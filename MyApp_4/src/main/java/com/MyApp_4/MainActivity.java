bfe4ec72ea8f9aff8de1dcef5bd0873ecfa1065c
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2020-10-28 14:34:04 +0200

    Урок 4. SoundPool при клике по кнопке играют одновременно 2 звука

--- MyApp_4/src/main/java/com/MyApp_4/MainActivity.java
+++ MyApp_4/src/main/java/com/MyApp_4/MainActivity.java
@@ -1,14 +1,38 @@
 package com.MyApp_4;
 
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
+    private SoundPool sp;
+    private int soundID1;
+    private int soundID2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sp = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+        soundID1 = sp.load(this, R.raw.alarm, 1);
+        soundID2 = sp.load(this, R.raw.alert, 1);
+
+        Button sound = findViewById(R.id.sound);
+        sound.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        play(soundID1);
+        play(soundID2);
+    }
+
+    private void play(int soundID) {
+        sp.play(soundID, 1, 1, 0, 0, 1);
     }
 }
